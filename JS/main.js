d1d2c2ecaa453d2e5f645c4b57beeeb033b36ff4
Eduard Cabrero <eduardc.herrero@gmail.com>
2022-02-07 19:48:43 +0100

    Play again

--- JS/main.js
+++ JS/main.js
@@ -1,49 +1,35 @@
 // * GLOBAL VARIABLES
 
 let gameIntro = document.querySelector("#game-intro");
-let gameOverScreen = document.querySelector("#gameover-screen")
+let gameOverScreen = document.querySelector("#gameover-screen");
 let canvas = document.querySelector("#my-canvas");
-let ctx = canvas.getContext("2d")
+let ctx = canvas.getContext("2d");
 let newGame;
-//let loseBtn = document.querySelector("#lost-btn")
-
 
 // * STATE MANAGEMENT FUNCTIONS
 
 const startGame = () => {
 
-  // desaparer el game intro screen y aparecer canvas
   gameIntro.style.display = "none";
   canvas.style.display = "flex";
-  //loseBtn.style.display = "flex";
+  gameOverScreen.style.display = "none";
 
-
-  // ejecutar mi juego (aqui esta todo lo que estamos 
   newGame = new Game();
-  newGame.gameLoop(); 
+  newGame.gameLoop();
+};
 
-}
-
-// const loseGame = () => {
-//   canvas.style.display = "none";
-//   loseBtn.style.display = "none";
-//   gameOverScreen.style.display = "flex";
-//   window.setTimeout(function(){location.reload()}, 4000);
-
-// }
+const playAgain = () => {
+  window.location.reload();
+};
 
 // * ADD EVENT LISTENERS
 
-let startButton = document.querySelector("#start-btn")
-startButton.addEventListener("click", startGame)
+let startButton = document.querySelector("#start-btn");
+startButton.addEventListener("click", startGame);
 
 document.addEventListener("keydown", (event) => {
-    newGame.player.mzMovement(event);
+  newGame.player.mzMovement(event);
 });
 
-// let loseBtn = document.querySelector("#lost-btn")
-// loseBtn.addEventListener("click", loseGame)
-
-
-
-
+let restart = document.querySelector("#restart-btn");
+restart.addEventListener("click", playAgain);
